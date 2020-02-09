24ef9bbb5e706ad7fe443528cf97e171d00e9142
adhemukhlis <adhemukhlis@gmail.com>
2020-02-09 22:49:40 +0700

    init

---
+++ precache-manifest.e10f06922560ae826e8c10611e246f59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ab4e9735b0c18e1b038810015ba32b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3899031606f874f5cd2d",
+    "url": "/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "3899031606f874f5cd2d",
+    "url": "/static/js/2.6e136559.chunk.js"
+  },
+  {
+    "revision": "699b0f4b5c4def540d5d7ef8683aaade",
+    "url": "/static/js/2.6e136559.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48d3bf06dd4fee3cb742",
+    "url": "/static/js/main.2489f1e6.chunk.js"
+  },
+  {
+    "revision": "0383c1f0ea75e00fb614",
+    "url": "/static/js/runtime-main.e5288802.js"
+  }
+]);
